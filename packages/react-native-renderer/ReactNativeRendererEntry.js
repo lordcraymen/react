d53bddf870d161733a568a12127ca6c81dbd1bcf
florian.patzke@t-online.de <florian.patzke@t-online.de>
2024-09-12 11:30:53 +0200

    Update dependencies to use local files

---
+++ packages/react-native-renderer/ReactNativeRendererEntry.js
@@ -0,0 +1,13 @@
+// packages/react-native-renderer/ReactNativeRendererEntry.js
+
+export { default as ReactNativeRenderer } from './src/ReactNativeRenderer';
+export { default as ReactNativeInjection } from './src/ReactNativeInjection';
+export { default as ReactNativeComponentTree } from './src/ReactNativeComponentTree';
+export { default as ReactNativePublicCompat } from './src/ReactNativePublicCompat';
+export { default as ReactNativeFiberInspector } from './src/ReactNativeFiberInspector';
+export { default as ReactGenericBatching } from './src/legacy-events/ReactGenericBatching';
+export { default as ReactNativeInjectionShared } from './src/ReactNativeInjectionShared';
+export { default as ReactNativeEventEmitter } from './src/ReactNativeEventEmitter';
+export { default as ReactNativeGlobalResponderHandler } from './src/ReactNativeGlobalResponderHandler';
+export { default as ResponderEventPlugin } from './src/legacy-events/ResponderEventPlugin';
+export { default as EventPluginUtils } from './src/legacy-events/EventPluginUtils';
